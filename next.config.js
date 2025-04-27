e179fcd550ff8ab39c8701a2655d2df3e12ae91e
yyitntg <yyitntg@gmail.com>
2025-04-27 12:55:28 +0800

    简化项目配置，移除所有不必要的构建配置

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    emotion: true,
-  },
-  // Cloudflare Pages 适配
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  swcMinify: true
 };
 
 module.exports = nextConfig; 
